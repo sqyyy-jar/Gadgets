cf18834018c37e8ff884207d754e61217bcc27f8
MCTzOCK <dev@mctzock.de>
2021-02-22 15:23:55 +0100

    Created basic Structure

---
+++ src/main/java/net/craftions/gadgets/Gadgets.java
@@ -0,0 +1,17 @@
+package net.craftions.gadgets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gadgets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
