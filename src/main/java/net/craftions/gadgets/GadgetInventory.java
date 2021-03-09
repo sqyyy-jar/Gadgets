378411f1a02701f25416651d3d513794d6da26a6
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2021-03-09 08:06:43 +0100

    Create GadgetInventory.java

---
+++ src/main/java/net/craftions/gadgets/GadgetInventory.java
@@ -0,0 +1,24 @@
+package net.craftions.gadgets;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class GadgetInventory {
+    public void open(Player player) {
+        Inventory inv = Bukkit.createInventory(null, 54, "§4§lGadgets");
+        if (Gadgets.GADGETS.size() > 54) {
+            player.sendMessage("§cThere are too many gadgets!");
+        } else {
+            for (Gadget gadget : Gadgets.GADGETS) {
+                ItemStack item = new ItemStack(gadget.getMaterial());
+                ItemMeta meta = item.getItemMeta();
+                meta.setDisplayName(gadget.getName());
+                item.setItemMeta(meta);
+                inv.addItem(item);
+            }
+        }
+    }
+}
